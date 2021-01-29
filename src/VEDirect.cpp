dc1d187ba36c789a222a075e176e8f9ceafe3940
TooT <theo.p-h-@live.fr>
2021-01-29 12:15:29 +0100

    Changing reading method to a non-blocking approach based on state machine presented on VE FAQ  --> https://www.victronenergy.com/live/vedirect_protocol:faq#framehandler_reference_implementation
    Adding MPPT and Phoenix labels

--- src/VEDirect.cpp
+++ src/VEDirect.cpp
@@ -14,103 +14,189 @@
 
 #include "VEDirect.h"
 
-VEDirect::VEDirect(HardwareSerial& port):
-	VESerial(port)
+VEDirect::VEDirect(HardwareSerial& port): mVESerial(port)
 	// Initialise the serial port that the
 	// VE.Direct device is connected to and
 	// store it for later use.
 {
+	mState = Idle;
+	mRX_Pointer = 0;
+	mCurrentLabel = VE_DUMP;
+	mChecksum = 0;
+	mNewData = false;
 }
 
-VEDirect::~VEDirect() {
-	// virtual destructor
-}
-
-uint8_t VEDirect::begin() {
-	// Check connection the serial port
-	VESerial.begin(19200);
-	if (VESerial) {
-		delay(500);
-		if(VESerial.available()) {
-			VESerial.flush();
-			VESerial.end();
+uint8_t VEDirect::begin()
+{
+	// Check connection with the serial port
+	mVESerial.begin(VED_BAUD_RATE);
+	if (mVESerial)
+	{
+		delay(1500);
+		if(mVESerial.available())
+		{
+			mVESerial.flush();
+			mVESerial.end();
 			return 1;
 		}
 	}
 	return 0;
 }
 
-int32_t VEDirect::read(uint8_t target) {
-	// Read VE.Direct lines from the serial port
-	// Search for the label specified by enum target
-	// Extract and return the corresponding value
-	// If value is "ON" return 1. If "OFF" return 0;
-
-	uint16_t loops = VED_MAX_READ_LOOPS;
-	uint8_t lines = VED_MAX_READ_LINES;
-	int32_t ret = 0;					// The value to be returned
-	char line[VED_LINE_SIZE] = "\0";	// Line buffer
-	uint8_t idx = 0;					// Line buffer index
-	char* label;
-	char* value_str;
-	int8_t b;							// byte read from the stream
-
-	VESerial.begin(VED_BAUD_RATE);
-
-	while (lines > 0) {
-		if (VESerial.available()) {
-			while (loops > 0) {
-				b = VESerial.read();
-				if ((b == -1) || (b == '\r')) { 	// Ignore '\r' and empty reads
-					loops--;
-				} else {
-					if (b == '\n') { 				// EOL
-						break;
-					} else {
-						if (idx < VED_LINE_SIZE) {
-							line[idx++] = b;		// Add it to the buffer
-						} else {
-							return 0;				// Buffer overrun
-						}
-					}
-				}
-			}
-			line[idx] = '\0';						// Terminate the string
-
-			// Line in buffer
-			if (target == VE_DUMP) {
-				// Diagnostic routine - just print to Serial0;
-				Serial.println(line);
-				// Continue on rather than break to reset for next line
-			}
-
-			label = strtok(line, "\t");
-			if (strcmp_P(label, ved_labels[target]) == 0) {
-				value_str = strtok(0, "\t");
-				if (value_str[0] == 'O') { 		//ON OFF type
-					if (value_str[1] == 'N') {
-						ret = 1;	// ON
-						break;
-					} else {
-						ret = 0;	// OFF
-						break;
-					}
-				} else {
-					sscanf(value_str, "%ld", &ret);
-					break;
-				}
-			} else {			// Line not of interest
-				lines--;
-				loops = VED_MAX_READ_LOOPS;
-				line[0] = '\0';
-				idx = 0;
-			}
-		}
-	}
-	return ret;
+bool VEDirect::available()
+{
+	return mNewData;
 }
 
-void VEDirect::copy_raw_to_serial0() {
-	read(VE_DUMP);
+int32_t VEDirect::read(uint8_t Label)
+{
+
+	int32_t value;
+
+	mNewData = false;
+
+	if(Label >= VE_LAST_LABEL)
+		return -1;
+
+	if (mData[Label][0] == 'O')
+	{
+		//ON OFF type
+		if (mData[Label][1] == 'N')
+			value = 1;	// ON
+
+		else
+			value = 0;	// OFF
+	}
+
+	else
+	{
+		sscanf(mData[Label], "%ld", &value);
+	}
+
+	return value;
+}
+
+void VEDirect::update()
+{
+	while(mVESerial.available())
+	{
+		uint8_t inbyte = mVESerial.read();
+
+		inbyte = toupper(inbyte);
+
+		switch(mState)
+		{
+			case Idle:
+				/* wait for \n of the start of an record */
+				switch(inbyte)
+				{
+					case '\n':
+						mState = Rec_Begin;
+						break;
+
+					case '\r': /* Skip */
+					default:
+						break;
+				}
+				break;
+
+			case Rec_Begin:
+				mRX_Pointer = 0;
+				mRX_Buffer[mRX_Pointer] = inbyte;
+				mRX_Pointer ++;
+
+				mState = Rec_Name;
+				break;
+
+			case Rec_Name:
+				// The record name is being received, terminated by a \t
+				switch(inbyte)
+				{
+					case '\t':
+						if (mRX_Pointer < VED_LINE_SIZE)
+						{
+							mRX_Buffer[mRX_Pointer] = 0; /* Zero terminate */
+
+							for(uint8_t i = 0; i < VE_LAST_LABEL; i++)
+							{
+								if (strcmp(mRX_Buffer, ved_labels[i]) == 0)
+								{
+									mCurrentLabel = i;
+									break;
+								}
+							}
+
+							if(mCurrentLabel == VE_CHECKSUM) // If label is checksum change state to checksum verification
+								mState = Checksum;
+							else if(mCurrentLabel == VE_LAST_LABEL) // Label hasn't been found, going back to Idle State
+								mState = Idle;
+							else
+								mState = Rec_Value;
+						}
+
+						else	// Buffer overflow going back to Idle
+							mState = Idle;
+
+						break;
+
+					default:
+						// add byte to name, and check overflow
+						if (mRX_Pointer< VED_LINE_SIZE)
+						{
+							mRX_Buffer[mRX_Pointer] = inbyte;
+							mRX_Pointer++;
+						}
+						else	// Buffer overflow going back to Idle
+							mState = Idle;
+
+						break;
+				}
+				break;
+
+			case Rec_Value:
+				// The record value is being received.  The \r indicates a new record.
+				switch(inbyte)
+				{
+					case '\n':
+						// forward record
+						if (mRX_Pointer < VED_MAX_VALUE_SIZE)
+						{
+							*mRX_Buffer = 0; // make zero ended
+
+							if(mCurrentLabel < VE_LAST_LABEL)	// Is selected Label correct ?
+							{
+								strcpy(mData[mCurrentLabel], mRX_Buffer);
+							}
+						}
+
+						mState = Rec_Begin;
+						break;
+
+					case '\r': /* Skip */
+						break;
+
+					default:
+						// add byte to name, and check overflow
+						if (mRX_Pointer< VED_LINE_SIZE)
+						{
+							mRX_Buffer[mRX_Pointer] = inbyte;
+							mRX_Pointer++;
+						}
+						else	// Buffer overflow going back to Idle
+							mState = Idle;
+						break;
+				}
+				break;
+
+			case Checksum:
+				bool valid = mChecksum == 0;
+				mChecksum = 0;
+				mState = Idle;
+				mNewData = true;
+				break;
+		}
+	}
+
+	//mVESerial.
 }
 
--- src/VEDirect.h
+++ src/VEDirect.h
@@ -23,11 +23,16 @@
 #include <Arduino.h>
 
 // Tunable parameters - defaults tested on mega2560 R3
-#define VED_LINE_SIZE 30		 // Seems to be plenty. VE.Direct protocol could change
-#define VED_MAX_LEBEL_SIZE 6	 // Max length of all labels of interest + '\0'. See ved_labels[]
+#define VED_LINE_SIZE 20		 // Seems to be plenty. VE.Direct protocol could change
+
+#define VED_MAX_LABEL_SIZE 10	 // Max length of value
+#define VED_MAX_VALUE_SIZE 10	 // Max length of value
+
 #define VED_MAX_READ_LOOPS 60000 // How many read loops to be considered a read time-out
 #define VED_MAX_READ_LINES 50	 // How many lines to read looking for a value
 								 // before giving up. Also determines lines for diag dump
+#define VED_RX_BUFFER_SIZE	50   // RX buffer size by line
+
 #define VED_BAUD_RATE 19200
 
 // Extend this and ved_labels[] for needed inclusions
@@ -38,27 +43,87 @@ enum VE_DIRECT_DATA {
 	VE_POWER,
 	VE_CURRENT,
 	VE_ALARM,
-	VE_LAST_LABEL,
+	VE_PANEL_VOLTAGE,
+	VE_PANEL_POWER,
+	VE_BATTERY_LOAD_CURRENT,
+	VE_LOAD_STATE,
+	VE_RELAY_STATE,
+	VE_ALARM_REASON,
+	VE_YIELD_TOTAL,
+	VE_YIELD_TODAY,
+	VE_MAX_DAILY_POWER,
+	VE_YIELD_YESTERDAY,
+	VE_MAX_YESTERDAY_POWER,
+	VE_ERROR_CODE,
+	VE_STATE_OF_OPERATION,
+	VE_FW,
+	VE_PID,
+	VE_DAY_SEQUENCE_NUMBER,
+	VE_DEVICE_MODE,
+	VE_AC_OUTPUT_VOLTAGE,
+	VE_AC_OUTPUT_CURRENT,
+	VE_WARNING_REASON,
+	VE_CHECKSUM,
+	VE_LAST_LABEL
 };
 
-const char ved_labels[VE_LAST_LABEL][VED_MAX_LEBEL_SIZE] PROGMEM = {
+const char ved_labels[VE_LAST_LABEL][VED_MAX_LABEL_SIZE] PROGMEM = {
 		"Dump",	// a string that won't match any label
 		"SOC",
 		"V",
 		"P",
 		"I",
 		"Alarm",
+		"VPV",
+		"PPV",
+		"IL",
+		"LOAD",
+		"Relay",
+		"AR",
+		"H19",
+		"H20",
+		"H21",
+		"H23",
+		"ERR",
+		"CS",
+		"FW",
+		"PID",
+		"HSDS",
+		"MODE",
+		"AC_OUT_V",
+		"AC_OUT_I",
+		"WARN",
+		"Checksum"
 };
 
-class VEDirect {
-public:
-	VEDirect(HardwareSerial& port);
-	virtual ~VEDirect();
-	uint8_t begin();
-	int32_t read(uint8_t target);
-	void copy_raw_to_serial0(); // kept for backwards compatibility
-private:
-	HardwareSerial& VESerial;
+class VEDirect
+{
+	public:
+		VEDirect(HardwareSerial& port);
+		virtual ~VEDirect();
+		uint8_t begin();
+		void update();
+
+		int32_t read(uint8_t Label);
+
+		bool available();
+
+
+	private:
+		HardwareSerial& mVESerial;
+
+		bool mNewData;
+
+		enum States{Idle, Rec_Begin, Rec_Name, Rec_Value, Checksum};
+
+		uint8_t mCurrentLabel;
+
+		char mData[VE_LAST_LABEL][VED_MAX_VALUE_SIZE];
+		char mRX_Buffer[VED_LINE_SIZE];
+		uint8_t mRX_Pointer;
+		uint8_t mState;
+		uint16_t mChecksum;
+
 };
 
 #endif /* VEDIRECT_H_ */
